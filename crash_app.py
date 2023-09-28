118eb97a9b6a2cce3aec98024a71f3d0271cacb3
dmropp <134107540+dmropp@users.noreply.github.com>
2023-09-28 13:36:04 -0700

    Fixing date format

--- crash_app.py
+++ crash_app.py
@@ -21,45 +21,59 @@ Crashes = Base.classes.oregon_crashes
 
 print(Base.classes.keys())
 
-session = Session(engine) # will need to comment this out when creating API
+# session = Session(engine) # will need to comment this out when creating API
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def welcome():
-#     session = Session(engine)
-#     # crash_list = session.query(Crashes.CRASH_ID).filter(Crashes.CRASH_EVNT_1_CD == 35).all()
-#     session.close()
+@app.route("/")
+def welcome():
+    session = Session(engine)
+    # crash_list = session.query(Crashes.CRASH_ID).filter(Crashes.CRASH_EVNT_1_CD == 35).all()
+    session.close()
 
-#     return (
-#         f"Welcome to our Oregon Crash App!"
-#         # jsonify(crash_dict)
-#     )
+    return (
+        f"Welcome to our Oregon Crash App!"
+        # jsonify(crash_dict)
+    )
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+@app.route("/api/v1.0/crash_map")
+def crash_map():
+    session = Session(engine)
+
+    results = session.query(Crashes.CRASH_DT).filter(Crashes.CRASH_EVNT_1_CD == 35).filter(dt.datetime.strptime(Crashes.CRASH_DT.tostring, "%m/%d/%Y") >= (dt.date(2019, 1, 1))).all()
+    # results = session.query(Crashes.CRASH_DT).filter(Crashes.CRASH_EVNT_1_CD == 35).all()
+    # results = session.query(Crashes.CRASH_ID).all()
+    
+    session.close()
+
+    print(len(results))
+    
+    deer_elk_crashes = np.ravel(results).tolist()
+
+    print(len(deer_elk_crashes))
+
+    return jsonify(deer_elk_crashes)
+    #return("hello")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
-inspector = inspect(engine)
-print(inspector.get_table_names())
+# inspector = inspect(engine)
+# print(inspector.get_table_names())
 # columns = inspector.get_columns('oregon_crashes')
 # for column in columns:
 #     print(column["name"], column["type"])
 
-# data = engine.execute("SELECT CRASH_ID FROM oregon_crashes WHERE CRASH_EVNT_1_CD = 035;")
-# data = engine.execute("SELECT CRASH_EVNT_SHORT_DESC FROM oregon_crashes")
-# deer_crashes = []
-# for record in data:
-#     deer_crashes.append(record)
-# print(len(deer_crashes))
+# crash_list = session.query(Crashes.LAT_DD).filter(Crashes.CRASH_EVNT_1_CD == 35).all()
+# print(crash_list)
+# date_list = session.query(Crashes.CRASH_DT).filter(Crashes.CRASH_EVNT_1_CD == 35).all()
+# print(date_list)
+# crash_list_array = []
+# for crash in crash_list:
+#     crash_list_array.append(crash)
 
-crash_list = session.query(Crashes.CRASH_ID).filter(Crashes.CRASH_EVNT_1_CD == 35).all()
-print(len(crash_list))
-crash_list_array = []
-for crash in crash_list:
-    crash_list_array.append(crash)
+# print(len(crash_list_array))
 
-print(len(crash_list_array))
-
-session.close() # will need to comment this out when creating API
+# session.close() # will need to comment this out when creating API
