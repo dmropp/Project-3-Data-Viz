d7bc075d4181ba1d964ae212a7164835a8f66fda
dmropp <134107540+dmropp@users.noreply.github.com>
2023-10-02 16:03:22 -0700

    Updated route names and removed debugging

--- crash_app.py
+++ crash_app.py
@@ -23,8 +23,6 @@ print(Base.classes.keys())
 
 app = Flask(__name__)
 
-#session = Session(engine) # will need to comment this out when creating API
-
 @app.route("/")
 def welcome():
     session = Session(engine)
@@ -33,13 +31,13 @@ def welcome():
     return (
         f"<h2 id='Welcome Page Header'>Welcome to our Oregon Crash App!</h2>" 
         f"<h3 id='Subheader'>Please use the following routes:</h3>"
-        f"<p>/api/v1.0/crash_map for json data of all car crashes in Oregon involving wild animals<br>"
-        f"/api/v1.0/dashboard for json data of all car crashes in Oregon involving wild animals<br>"
-        f"/api/v1.0/map_viz for an interactive map illustrating the location of all call crashes in Oregon involving wild animals <br>"
-        f"/api/v1.0/dashboard_viz for an interactive dashboard of statistics behind car crashes with wild animals in Oregon<p/>"
+        f"<p>/map_data for json data of all car crashes in Oregon involving wild animals<br>"
+        f"/dashboard_data for json data of all car crashes in Oregon involving wild animals<br>"
+        f"/map for an interactive map illustrating the location of all call crashes in Oregon involving wild animals <br>"
+        f"/dashboard for an interactive dashboard of statistics behind car crashes with wild animals in Oregon<p/>"
     )
 
-@app.route("/api/v1.0/crash_map")
+@app.route("/map_data")
 def crash_map():
     session = Session(engine)
 
@@ -125,12 +123,8 @@ def crash_map():
     print(len(animal_crashes))
 
     return jsonify(animal_crashes)
-    #return("hello")
-    # data = jsonify(animal_crashes)
-    # print(len(data))
-    # return render_template("map_index.html", data=jsonify(animal_crashes))
 
-@app.route("/api/v1.0/dashboard")
+@app.route("/dashboard_data")
 def dashboard():
 
     session = Session(engine)
@@ -217,100 +211,14 @@ def dashboard():
     print(len(animal_crashes))
           
     return jsonify(animal_crashes)
-    # return render_template("dashboard_index.html", data=jsonify(animal_crashes))
 
-# @app.route("/api/v1.0/map_viz") #see if I can create separate HTML file and use return(call HTML)
-# def map_viz():
-#     return(
-#         f"<html lang='en'>"
-#         f"<head>"
-#             f"<meta charset='UTF-8'>"
-#             f"<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-#             f"<meta http-equiv='X-UA Compatible' content='ie=edge'>"
-#             f"<title>Oregon Crash Map</title>"
-#             f"<link rel='stylesheet' href='https://unpkg.com/leaflet@1.9.4/dist/leaflet.css' integrity='sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=' crossorigin='' />" #leaflet stylesheet
-#             # f"<link rel='stylesheet' type='text/css' href='FILEPATH'>" - link to our CSS style sheet
-#         f"</head>" 
-#         f"<body>"
-#             f"<div id='map'></div>"
-#             f"<script src='https://unpkg.com/leaflet@1.9.4/dist/leaflet.js' integrity='sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=' crossorigin=''></script>" #leaflet script
-#             f"<script src='https://d3js.org/d3.v7.min.js'></script>" #D3 script
-#             #f"<script src='http://127.0.0.1:5000/api/v1.0/crash_map'></script>"
-#             #f"<script type='text/javascript' filename='.js/map_app_js.js'></script>" # link to our javascript
-#             f"<script type='text/javascript' src='js/map_app_js.js'></script>"
-#             #f"<script src='{{ url_for("static", filename='.js/map_app_js.js') }}></script>" # link to our javascript
-#         f"</body>"
-#     )
-
-@app.route("/api/v1.0/map_viz") #see if I can create separate HTML file and use return(call HTML)
+@app.route("/map")
 def map_viz():
     return render_template("map_index.html")
-    
-# Dashboard visualization page
-# @app.route("/api/v1.0/dashboard_viz")
-# def dashboard_viz():
-#     return(
-#         f"<html lang='en'>"
-#         f"<head>"
-#             f"<meta charset='UTF-8'>"
-#             f"<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-#             f"<meta http-equiv='X-UA-Compatible' content='ie=edge'>"
-#             f"<title>Oregon Crash Dashboard</title>"
-#             f"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>" # if we need custom CSS we can add the file to the head section
-#         f"</head>"
-#         f"<body>"
-#             f"<div class='container'>"
-#                 f"<div class='row'>"
-#                     f"<div class='col-md-12 jumbotron text-center'>"
-#                         f"<h1>Dashboard goes here</h1>"
-#                         f"<h4>Use the interactive charts below to explore the dataset</h4>"
-#                     f"</div>"
-#                 f"</div>"
-#                 f"<div class='row'>"
-#                     f"<div class='col-md-2'>"
-#                         f"<div class='well'>"
-#                             f"<h5>Date:</h5>"
-#                             f"<select id='selDate' onchange='optionChanged(this.value)'></select>" # Used the same code from the belly button challenge to handle the drop down menu
-#                         f"</div>"
-#                     f"</div>"
-#                     f"<div class='col-md-5'>"
-#                         f"<div id='bar'></div>"
-#                     f"</div>"
-#                     f"<div class='col-md-5'>"
-#                         f"<div id='plot'></div>"
-#                     f"</div>"
-#                 f"</div>"
-#             f"</div>"
-#             f"<script src='https://d3js.org/d3.v7.min.js'></script>" #D3 script 
-#             f"<script src='https://cdn.plot.ly/plotly-latest.min.js'></script>" #Plotly script
-#             #f"<script src='FILEPATH'></script>" our javascript file
-#         f"</body>"   
-#         f"</html>"         
-#     )
 
-@app.route("/api/v1.0/dashboard_viz")
+@app.route("/dashboard")
 def dashboard_viz():
     return render_template("dashboard_index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# debugging
-# inspector = inspect(engine)
-# print(inspector.get_table_names())
-# columns = inspector.get_columns('oregon_crashes')
-# for column in columns:
-#     print(column["name"], column["type"])
-
-# crash_list = session.query(Crashes.LAT_DD).filter(Crashes.CRASH_EVNT_1_CD == 35).all()
-# print(len(crash_list))
-# date_list = session.query(Crashes.CRASH_DT).filter(Crashes.CRASH_EVNT_1_CD == 35).all()
-# print(date_list)
-# crash_list_array = []
-# for crash in crash_list:
-#     crash_list_array.append(crash)
-
-# print(len(crash_list_array))
-
-# session.close() # will need to comment this out when creating API
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,19 +1,10 @@
 //https://towardsdatascience.com/talking-to-python-from-javascript-flask-and-the-fetch-api-e0ef3573c451, add to readme for general project reference
 
-var dashboardQueryURL = "http://127.0.0.1:5000/api/v1.0/dashboard";
-//var dashboardQueryURL = "http://localhost:5000/api/v1.0/dashboard";
-//var dashboardQueryURL = "/api/v1.0/dashboard";
+var dashboardQueryURL = "http://127.0.0.1:5000/dashboard_data";
 
-// D3 call for map viz
+// D3 call for dashboard data
 d3.json(dashboardQueryURL).then(function(data) { // https://stackoverflow.com/questions/53716527/request-data-from-front-end-d3-json-from-python-flask-backend, referenced for how to call flask data from javascript
-    //console.log(data);
-    //createMarkers(data) 
+    
     console.log("Hello");
     console.log(data);
 });
-
-//console.log("hello")
-
-// fetch(dashboardQueryURL, {mode: "no-cors"}).then(function(data) {
-//     console.log(data);
-// });
